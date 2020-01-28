20b81b45f90d8e183e21e213b3178c164ee5c9fd
Luke Mico <lukemico@me.com>
2020-01-28 12:50:56 +1100

    Create Filters

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ var card = new Vue({
 			{ text: 'Velociraptor' },
 			{ text: 'Triceratops' },
 			{ text: 'Stegosaurus' }
+		],
+		dinos: [
+			{
+				text: 'Velociraptor',
+				weight: '15 kg'
+			},
+			{
+				text: 'triceratops',
+				weight: '6,000 kg'
+			},
+			{
+				text: 'Stegosaurus',
+				weight: '2,500 kg'
+			}
 		]
 	},
 	methods: {
@@ -21,6 +35,23 @@ var card = new Vue({
 		},
 		deleteItem: function(index) {
 			this.items.splice(index, 1);
+		}
+	},
+	filters: {
+		capitalize: function(value) {
+			if (!value) return '';
+			value = value.toString();
+			return value.charAt(0).toUpperCase() + value.slice(1);
+		},
+		undercase: function(value) {
+			if (!value) return '';
+			value = value.toString();
+			return value.toLowerCase();
+		},
+		url: function(value) {
+			if (!value) return '';
+			value = value.toString();
+			return 'https://en.wikipedia.org/wiki/' + value;
 		}
 	}
 });
