71da87045444ef9aa1d92157745a51511bc4e3e4
Luke Mico <lukemico@me.com>
2020-01-28 12:21:11 +1100

    Create a Basic Component using Vue.js

--- script.js
+++ script.js
@@ -1,37 +1,8 @@
 var card = new Vue({
 	el: '#card',
 	data: {
-		title: 'dinosaurs',
-		dinos: [
-			{
-				text: 'Velociraptor',
-				weight: '15 kg'
-			},
-			{
-				text: 'triceratops',
-				weight: '6,000 kg'
-			},
-			{
-				text: 'Stegosaurus',
-				weight: '2,500 kg'
-			}
-		]
-	},
-	filters: {
-		capitalize: function(value) {
-			if (!value) return '';
-			value = value.toString();
-			return value.charAt(0).toUpperCase() + value.slice(1);
-		},
-		undercase: function(value) {
-			if (!value) return '';
-			value = value.toString();
-			return value.toLowerCase();
-		},
-		url: function(value) {
-			if (!value) return '';
-			value = value.toString();
-			return 'https://en.wikipedia.org/wiki/' + value;
-		}
+		title: 'Dinosaurs',
+		content:
+			'<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period.'
 	}
 });
